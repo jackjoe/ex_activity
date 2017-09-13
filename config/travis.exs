b2bc2de2aa956f8b38c420f23c0629eb2e3db994
Jeroen Bourgois <jeroenbourgois@gmail.com>
2017-09-13 21:20:08 +0200

    travis

--- config/travis.exs
+++ config/travis.exs
@@ -3,5 +3,7 @@ use Mix.Config
 config :ex_activity, ExActivity.Repo,
   adapter: Ecto.Adapters.MySQL,
   database: "ex_activity_test",
+  username: "travis",
+  password: "",
   hostname: "127.0.0.1",
   pool: Ecto.Adapters.SQL.Sandbox
